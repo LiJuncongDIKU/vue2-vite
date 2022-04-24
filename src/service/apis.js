02d3670691e2764f10ee5f6f36f26ae2209eb229
LiJuncongDIKU <ljc1350220@163.com>
2022-04-24 09:28:16 +0800

    [feat] 更新ljc-axios的用法

--- src/service/apis.js
+++ src/service/apis.js
@@ -3,10 +3,10 @@
  * 
  * https://www.npmjs.com/package/ljc-axios
  */
-import { createGroup } from 'ljc-axios';
+import { createAxios } from 'ljc-axios';
 import eventBus from '@/plugins/eventBus';
 
-const server = createGroup('/api/xxxxxxxx');
+const server = createAxios({ host: '/api/xxxxxxxx' });
 
 // 定义的组都写在数组中，通用的过滤器使用
 const groups = [server];
@@ -19,31 +19,17 @@ export default {
 groups.forEach((axiosInstance) => {
   // 响应过滤
   axiosInstance.interceptors.response.use((res) => {
-    const data = res.data;
-    if (!data) {
-      return res;
-    }
-    const { code, message } = data;
-    if (!code || code === '000') {
-      return data;
-    }
-    eventBus.$emit('request-error', { content: message, title: '请求错误' });
-    return data;
+    // 响应数据过滤
+    // const data = res.data;
+    // const { code, message } = data;
+    // if (true) {
+    //   return data;
+    // }
+    // eventBus.$emit('request-error', { content: message, title: '请求错误' });
+    return res.data;
   }, (e) => {
     if (e instanceof Error) { // 判断是网络问题还是取消请求
       eventBus.$emit('request-error', { content: '网络异常，无法连接到服务器', title: '网络异常' });
     }
-  });
-
-  // GET 请求去掉undefined和空字符串
-  axiosInstance.interceptors.request.use((req) => {
-    // 清空 param 中的 undefined 和空字符串
-    Object.keys(req.params || {}).forEach((key) => {
-      const val = req.params[key];
-      if (val === undefined || val === '') {
-        delete req.params[key]
-      }
-    });
-    return req;
   });
 });
